c1b89a00f156642a3dafc9821157543f2c372743
TestGTR <84667575+TestGTR@users.noreply.github.com>
2022-09-08 09:22:21 +0200

    try literal_eval

--- scripts/json_reader.py
+++ scripts/json_reader.py
@@ -1,7 +1,8 @@
 import sys
 import json
+import ast
 
-data = json.loads(sys.argv[1])
+data = json.loads(ast.literal_eval(sys.argv[1]))
 print(data)
 
 with Path("rdp_news_submitted.json").open("w") as out_file:
