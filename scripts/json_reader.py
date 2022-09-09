dda2620927471a0157075b10304033d9b51521f7
TestGTR <84667575+TestGTR@users.noreply.github.com>
2022-09-09 22:45:27 +0200

    only print

--- scripts/json_reader.py
+++ scripts/json_reader.py
@@ -2,13 +2,14 @@ import sys
 import json
 import ast
 
+
 input_json: str = sys.argv[1]
 print(input_json)
 
-data = json.loads(ast.literal_eval(sys.argv[1]))
-print(data)
+# data = json.loads(ast.literal_eval(sys.argv[1]))
+# print(data)
 
-with Path("rdp_news_submitted.json").open("w") as out_file:
-    out_file.write(data)
+# with Path("rdp_news_submitted.json").open("w") as out_file:
+#     out_file.write(data)
 
 
